327aa9a4fe0eb63d790b826b910b9f75c2ccd4bc
Igor Konnov <igor@konnov.phd>
2024-10-24 08:59:50 +0000

    fix the experiments script

--- spec4b-optimizations/run-experiments.sh
+++ spec4b-optimizations/run-experiments.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$APALACHE_HOME" == "" ]; then
+    echo "set APALACHE_HOME to the Apalache installation directory"
+    exit 1
+fi
+
 parallel --joblog joblog.txt --results out/ --colsep ',' \
     -a exp.csv \
-    /usr/bin/time -f "'%Uuser %Ssystem %elapsed %Mmaxk'" apalache-mc check --init={2} --inv={3} {1}
+    /usr/bin/time -f "'%Uuser %Ssystem %elapsed %Mmaxk'" ${APALACHE_HOME}/bin/apalache-mc check --init={2} --inv={3} {1}
