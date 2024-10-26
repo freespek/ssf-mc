7d161b6d75da0c00dc59719776cb0fcc91553cc0
Igor Konnov <igor@konnov.phd>
2024-10-26 12:26:20 +0200

    add experiments to check inductiveness

--- spec4b-optimizations/check-accountable-safety.sh
+++ spec4b-optimizations/check-accountable-safety.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# Run the experiments to check accountable safety.
 
 if [ "$APALACHE_HOME" == "" ]; then
     echo "set APALACHE_HOME to the Apalache installation directory"
@@ -6,5 +8,5 @@ if [ "$APALACHE_HOME" == "" ]; then
 fi
 
 parallel --joblog joblog.txt --results out/ --colsep ',' \
-    -a exp.csv \
+    -a experiments-accountable-safety.csv \
     /usr/bin/time -f "'%Uuser %Ssystem %elapsed %Mmaxk'" ${APALACHE_HOME}/bin/apalache-mc check --init={2} --inv={3} {1}
